3d89968c27af1b586fa9217c07adb3a515e634b1
Elmseld <emily.elmseld@gmail.com>
2016-02-25 13:18:32 +0100

    add all

---
+++ README.md
@@ -0,0 +1,56 @@
+# elms-eflashmessage
+
+Spara meddelanden i session och visar dem vid anrop. Används i Anax-MVC
+
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/Elmseld/elms-eflashmessage/badges/quality-score.png?b=kmom06)](https://scrutinizer-ci.com/g/Elmseld/elms-eflashmessage/?branch=kmom06)
+[![Code Coverage](https://scrutinizer-ci.com/g/Elmseld/elms-eflashmessage/badges/coverage.png?b=kmom06)](https://scrutinizer-ci.com/g/Elmseld/elms-eflashmessage/?branch=kmom06)
+[![Build Status](https://travis-ci.org/Elmseld/elms-eflashmessage.svg?branch=kmom06)](https://travis-ci.org/Elmseld/elms-eflashmessage)
+
+## Installation
+
+Installera med Packagist eller clona koden från Github source:
+```
+"elms/eflashmessage": "dev-master"
+```
+
+Lägg till följande kod för att kunna anropa en FlashController i DI i ditt Anax MVC framework :
+```
+$di->set('FlashController', function() use ($di) {
+    $flashController = new \Anax\FlashMessages\FlashController();
+    $flashController->setDI($di);
+    return $flashController;
+});
+```
+eller 
+* lägg in filen webroot/testflash.php i Anaxs webroot 
+* och filen css/EFlashMessage.css i webroot/css
+* Och kör igång och testa de olika meddelandena!
+
+I filen webroot/testflash.php hittar ni de fyra olika anrop som kan göra, för info, alert, success och error medelanden;
+
+För att spara meddelandena i session -
+Error meddelande:
+```
+$app->msg->error('Error flash meddelande!!');
+```
+Success meddelande:
+```
+$app->msg->success('Success flash meddelande!!');
+```
+Info meddelande:
+```
+$app->msg->info('Info flash meddelande!!');
+```
+alert meddelande:
+```
+$app->msg->alert('Alert flash meddelande!!');
+```
+
+För att sedan skriva ut meddelenadet använd: 
+```
+$app->theme->setVariable('title', "Flash messages")->setVariable('main', $app->msg->outputMsgs());
+```
+Och för att tömma sessionen på meddelanden använd;
+```
+$app->msg->clearMsg();
+```
---
+++ src/EFlashMessage/EFlashMessage.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Elms\EFlashMessage;
+
+
+/**
+ * Elms\EFlashMessage\EFlashMessage
+ *
+ * Denna klass skapar en funktion för flash meddelande.
+ */
+
+    class EFlashMessage {
+			
+				/**
+				 * Initalerar kontrollen
+				 * 
+				 * @return void
+				 */
+        public function __construct() {
+    			if(!isset($_SESSION['msg'])) {
+           	$_SESSION['msg'] = array();
+        	}
+				}
+			
+					/**
+					 * Lägg till ett alert meddelande
+					 *
+					 * @param $message string med meddelandets test
+					 *
+					 * @return void
+					 */
+				public function alert($content) {
+					$message = array('content' => $content, 'type' => 'alert');
+					$_SESSION['msg'][] = $message;
+				}
+
+
+					/**
+					 * Lägg till ett sucess meddelande
+					 *
+					 * @param $message string med meddelandets test
+					 *
+					 * @return void
+					 */
+				public function success($content) {
+					$message = array('content' => $content, 'type' => 'success');
+					$_SESSION['msg'][] = $message;
+				}
+
+					/**
+					 * Lägg till ett error meddelande
+					 *
+					 * @param $message string med meddelandets test
+					 *
+					 * @return void
+					 */
+				public function error($content) {
+					$message = array('content' => $content, 'type' => 'error');
+					$_SESSION['msg'][] = $message;
+				}
+
+				/**
+				 * Lägg till ett informations meddelande
+				 *
+				 * @param $message string med meddelandets test
+				 *
+				 * @return void
+				 */
+				public function info($content) {
+					$message = array('content' => $content, 'type' => 'info');
+					$_SESSION['msg'][] = $message;
+				}
+
+
+				/**
+	 			 * Prints the messages in the session flasher
+	 			 */
+				public function outputMsgs(){
+						$messages = $_SESSION['msg'];
+						$output = null;
+						if($messages) {
+								foreach ($messages as $key => $message) {
+										$output .= "<div class='" . $message['type'] . "'><p>" . $message['content'] . "</p></div>";
+								}
+						}
+						return $output;
+
+				}
+
+				/**
+	 			 * Clear messages in the session messenger
+	       */
+				public function clearMsg() {
+					$_SESSION['msg'] = null;
+				} 
+}
